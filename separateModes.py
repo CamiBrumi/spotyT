42b97170e13e35d7387c2080cf532a8c5815de95
petrakumi12 <pkumi@wpi.edu>
2019-04-13 13:05:52 -0400

    subset selection

--- separateModes.py
+++ separateModes.py
@@ -3,4 +3,5 @@ filename = 'SpotifyFeatures.csv'
 names = ['genre','artist_name','track_name','track_id','popularity','acousticness','danceability','duration_ms','energy','instrumentalness','key','liveness','loudness','mode','speechiness','tempo','time_signature','valence']
 df = pd.read_csv(filename)
 df.replace(["Minor", "Major"], [-1, 1], inplace = True)
-print(df)
+print(df['mode'])
+print(len(df))
